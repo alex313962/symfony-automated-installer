b26d6f121a9470ceca821147917fcbeff373b067
alex313962 <alex313962@gmail.com>
2021-05-23 01:41:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Symfony automated installer
+
+## Requirements
+
+Supported distributions:
+
+- Ubuntu >= 18.04
+
+## Usage
+
+Download and execute the script. Answer the questions asked by the script and it will take care of the rest.
+
+```bash
+curl -O https://raw.githubusercontent.com/alex313962/symfony-automated-installer/master/symfony_install.sh
+chmod +x symfony_install.sh
+./symfony_install.sh
+```
+
+It will install Symfony and it's dependecies on the system.
+
+## Tested in 
+
+- WSL2 Ubuntu 20.04
---
+++ symfony_install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+cd ~/
+
+
+sudo apt update
+
+while true; do
+    read -p "Do you want to do a full-upgrade?" upgrade
+    case $upgrade in
+        [Yy]* ) sudo apt full-upgrade -y; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+ 
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php -y
+sudo add-apt-repository ppa:ondrej/apache2 -y
+sudo apt update
+while true; do
+    read -p "Which php version you want to install?" version
+    case $version in
+        "7.4" )
+
+         echo 7.4; break;;
+        "8.0" )
+        
+         echo 8.0; break;;
+        * ) echo "Please answer 7.4 or 8.0";;
+    esac
+done
+
+sudo apt install php$version apache2 wget git php$version-xml php$version-mbstring php$version-intl php$version-mysql
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php 
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+wget https://get.symfony.com/cli/installer -O - | bash
+sudo mv ~/.symfony/bin/symfony /usr/local/bin/symfony
+symfony check:requirements
+echo "if symfony check is successfull, you can start to use Symfony"
+echo "and create new project with "
+echo "symfony new your-awesome-project-name --full"

1e623a80dd102cc713998430e16416fb0ac28db0
alex313962 <alex313962@gmail.com>
2021-08-09 17:06:09 +0200

    Update symfony_install_arch.sh

--- symfony_install_arch.sh
+++ symfony_install_arch.sh
@@ -10,8 +10,8 @@ sudo pacman-key --init
 sudo pacman-key --populate archlinux
 
 # install chaotic-aur
-sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 sudo pacman -Sy
 

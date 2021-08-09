f231a75f5a389344f2e3017e639569ab13cc9075
alex313962 <alex313962@gmail.com>
2021-08-09 05:54:06 +0200

    updated comment and fixed some issues

--- symfony_install_arch.sh
+++ symfony_install_arch.sh
@@ -2,16 +2,21 @@
 #!/bin/sh
 cd ~/
 
-
+#install git
 sudo pacman -Sy --needed git
 
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# install chaotic-aur
+pacman-key --lsign-key 3056513887B78AEB
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -Sy
+
 echo '[chaotic-aur]' | tee -a /etc/pacman.conf
 echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | tee -a /etc/pacman.conf
 echo '' | tee -a /etc/pacman.conf
 
+# question about full upgrade
 while true; do
     read -p "Do you want to do a full-upgrade?" upgrade
     case $upgrade in
@@ -21,6 +26,7 @@ while true; do
     esac
 done
 
+#if the user doesn't have paru, install it 
 if ! command -v paru &> /dev/null ; then
   sudo pacman -S --needed base-devel
   git clone https://aur.archlinux.org/paru.git
@@ -30,6 +36,7 @@ if ! command -v paru &> /dev/null ; then
   rm -r paru
 fi
  
+#check what version the user want and save it into $version 
 while true; do
     read -p "Which php version you want to install?" version
     case $version in
@@ -42,19 +49,28 @@ while true; do
         * ) echo "Please answer 7.4 or 8.0";;
     esac
 done
+
+#download php apache wget and phpintl
+paru -Sy --needed php$version apache wget php$version-intl 
+
+#download, install and clean composer from the original repository
 php$version -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php$version -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php$version composer-setup.php 
 php$version -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
-paru -Sy --needed php$version apache wget php$version-intl 
-sudo sed -i 's/;extension=iconv/extension=iconv/' /etc/php/php.ini
-sudo sed -i 's/;extension=intl/extension=intl/' /etc/php/php.ini
 
+#enable in the correct php.ini file the extensions required
+sudo sed -i 's/;extension=iconv/extension=iconv/' /etc/php$version/php.ini
+sudo sed -i 's/;extension=intl/extension=intl/' /etc/php$version/php.ini
+
+#get symfony via wget (original documentation)
 wget https://get.symfony.com/cli/installer -O - | bash
 sudo mv ~/.symfony/bin/symfony /usr/local/bin/symfony
 symfony check:requirements
 sudo mkdir /var/www/git
+
+#check if the user that is installing symfony is the user that will use symfony
 
 while true; do
     read -p "is the user $USER the owner of this symfony installation/project?" default_user
